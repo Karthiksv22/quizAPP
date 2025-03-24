cbf2cae429dac129d7cbcf49c38d2585cdb1ecda
Karthik S V <ksv1844@gmail.com>
2025-03-24 22:32:58 +0530

    Project added commit - 1

---
+++ src/test/java/com/quiz/quizapp/GeneralQuizappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quiz.quizapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeneralQuizappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
