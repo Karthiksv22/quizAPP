8fbaf13ba03ec5b9ca7703e84766702f6782b75d
Karthik S V <ksv1844@gmail.com>
2025-03-29 01:19:57 +0530

    Added swagger api for the current post controller

--- src/main/java/com/quiz/quizapp/PostController.java
+++ src/main/java/com/quiz/quizapp/PostController.java
@@ -1,8 +1,18 @@
 package com.quiz.quizapp;
 
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 @RestController
 public class PostController {
+
+    @RequestMapping(value = "/")
+    public void redirect (HttpServletResponse response) throws IOException {
+        response.sendRedirect("/swagger-ui.html");
+    }
 }
